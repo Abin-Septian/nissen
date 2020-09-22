1e91e46c24c567bbc5488be96061d87cd7d68545
Bintang Septiandaru <bintangseptiandaru@gmail.com>
2020-09-22 14:57:27 +0700

    dev

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,7 +16,7 @@ const tailwindcss = require('tailwindcss')
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js/app.js')
   .postCss('resources/css/app.css', 'public/css/app.css')
   .options({
     postCss: [
